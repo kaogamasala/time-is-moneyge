c919a87c418fac577715cfc108e36afadb946162
Norihiko Suehiro <coooooooolasacucumber@gmail.com>
2019-05-13 17:37:30 +0900

    ALLOWED_HOSTS変更-5

--- time_is_moneyge/settings.py
+++ time_is_moneyge/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['timeismoneyge.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'timeismoneyge.herokuapp.com']
 
 # django-crispy-forms 設定
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
