9637c3567dab18fdb586088f79e17c2cee578fdc
Norihiko Suehiro <coooooooolasacucumber@gmail.com>
2019-06-11 20:30:59 +0900

    再デプロイ

--- time_is_moneyge/settings.py
+++ time_is_moneyge/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #20190611
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') #20190611
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"), #static
